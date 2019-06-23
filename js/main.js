d76244ccaf04a6dd3df06327a5befc42c4b69509
Tong Su <99ttss@gmail.com>
2019-06-23 16:40:45 -0400

    Theme ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
 $(document).ready(function() {
     $('body').fadeIn(1000).removeClass('hidden');
+
+    // $('#skills').on('hide.bs.modal', function(e) {
+    //     $('body').attr('class', 'blurOut')
+    //     $('#skills').attr('class', 'modal blurOut')
+    //     console.log("asdf")
+    // })
+
+    $('.modal').on('hide.bs.modal', function(e) {
+        $('body').attr('class', 'blurOut');
+        $(this).attr('class', 'modal shown fadeOut');
+        setTimeout(function() {
+            $('body').attr('class', '');
+            $('.modal').attr('class', 'modal');
+        }, 100)
+    })
+
+    $('.toSkills').click(function(){
+        setTimeout(function() {
+            $('#navSkills').click()
+        }, 200)
+    })
+
+    $('.toAbout').click(function(){
+        setTimeout(function() {
+            $('#navAbout').click()
+        }, 200)
+    })
+
+    $('.toProjects').click(function(){
+        setTimeout(function() {
+            $('#navProjects').click()
+        }, 200)
+    })
+
+    $('.toWork').click(function(){
+        setTimeout(function() {
+            $('#navWork').click()
+        }, 200)
+    })
+
+    // $('.modal').on('hidden.bs.modal', function(e) {
+    //     $('body').attr('class', '')
+    //     $(this).attr('class', 'modal')
+    // })
+
+    // $('#projects').on('hide.bs.modal', function(e) {
+    //     $('#projects .modal-dialog').attr('class', 'modal-dialog blurOut')
+    // })
+    //
+    // $('#work').on('hide.bs.modal', function(e) {
+    //     $('#work .modal-dialog').attr('class', 'modal-dialog blurOut')
+    // })
+    //
+    // $('#about').on('hide.bs.modal', function(e) {
+    //     $('#about .modal-dialog').attr('class', 'modal-dialog blurOut')
+    // })
 });
