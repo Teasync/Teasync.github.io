7e66a65578381aeb48d7ac211cd886ef9904ace5
Tong Su <99ttss@gmail.com>
2019-06-25 16:28:11 -0400

    fix modal close event handlers taking over from sepcified ones

--- js/main.js
+++ js/main.js
@@ -1,38 +1,61 @@
-$(document).ready(function() {
-    $('body').fadeIn(1000).removeClass('hidden');
+$(document).ready(function () {
+    body = $('body');
+    body.fadeIn(1000).removeAttr('class').removeAttr('style');
 
-    $('.modal').on('hide.bs.modal', function(e) {
-        $('body').attr('class', 'blurOut');
-        $(this).attr('class', 'modal shown fadeOut');
-        setTimeout(function() {
-            $('body').attr('class', '');
-            $('.modal').attr('class', 'modal');
-        }, 100)
-    })
+    $(document).on('click', '.modal.show', function (e) {
+        if ($(e.target).hasClass('show')) {
+            handleClose(e);
+        }
+    });
 
-    $('.toSkills').click(function(){
-        setTimeout(function() {
+    $('.nav-it').on('click', function(e) {
+        window.setTimeout(function() {
+            $('.modal').off();
+            console.log('shown');
+        }, 300);
+    });
+
+    $("[data-dismiss-custom='modal']").on('click', handleClose);
+
+    $('.toSkills').click(function () {
+        handleClose()
+        setTimeout(function () {
             $('#navSkills').click()
         }, 200)
-    })
+    });
 
-    $('.toAbout').click(function(){
-        setTimeout(function() {
+    $('.toAbout').click(function () {
+        handleClose()
+        setTimeout(function () {
             $('#navAbout').click()
         }, 200)
-    })
+    });
 
-    $('.toProjects').click(function(){
-        setTimeout(function() {
+    $('.toProjects').click(function () {
+        handleClose()
+        setTimeout(function () {
             $('#navProjects').click()
         }, 200)
-    })
+    });
 
-    $('.toWork').click(function(){
-        setTimeout(function() {
+    $('.toWork').click(function () {
+        handleClose()
+        setTimeout(function () {
             $('#navWork').click()
         }, 200)
     })
-
-
 });
+
+function handleClose(e) {
+    //e.preventDefault();
+    console.log('dismissed');
+    modal = $('.modal.show');
+    modal.removeClass('show');
+    $('.modal-open').removeAttr('class');
+
+    modal.on('transitionend webkitTransitionEnd oTransitionEnd', function (e) {
+        $('.modal').modal('hide');
+        console.log('hidden');
+        $('.modal').off();
+    });
+}
